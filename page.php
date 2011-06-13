4bb210b17403f2d727599ca29ca1743d98de6080
uokesita <osledybazo@gmail.com>
2011-06-12 19:53:41 -0430

    fixes

--- page.php
+++ page.php
@@ -1,16 +1,6 @@
 <?php get_header(); ?>
 
 
-<div class="page-top">
-  <div class="boxes">
-    <div class="box left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim volutpat dolor sit amet convallis mattis</div>
-    <div class="box center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim volutpat dolor sit amet convallis mattis</div>
-    <div class="box right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim volutpat dolor sit amet convallis mattis</div>
-    <div class="clear"></div>
-  </div>
-</div>
-
-
 <div class="page-body">
 
   <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
